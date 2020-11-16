b9ea0c252f25433ec3d9578bfb73ddf7acb37a63
Rbloemhard <56073780+Rbloemhard@users.noreply.github.com>
2020-11-16 19:48:16 +0100

    Add files via upload

---
+++ opdrachten-lvl-13/opdracht-gebruik-if-else-statement/script.js
@@ -0,0 +1,28 @@
+const age = 15;
+
+if (age > 17) {
+
+    console.log("Please come in.");
+
+} else {
+
+    console.log("Not this time.");
+}
+
+const isFemale = true;
+const isMale = false;
+
+if (isFemale) {
+
+    console.log("Lets party.Ladies night it is!");
+} else {
+    console.log("Ladies only. Sorry not this time!");
+}
+
+const driverStatus = 'gerrit';
+if (driverStatus == 'bob') {
+
+    console.log("You're the bob, please drive.");
+} else {
+    console.log("You're drunk, please take a nap on the backseat.")
+}
